8ad3e0aa9d1208b24c4f4625e8e7692cd8c700b4
Vlastimil Zeman <zeman.vlastimil@gmail.com>
2019-10-22 11:46:36 +0100

    Add dependency of helm on its kubernetes account

--- helm.tf
+++ helm.tf
@@ -2,4 +2,6 @@ resource "helm_release" "wordpress" {
   name    = "wordpress"
   chart   = "stable/wordpress"
   version = "7.5.0"
+
+  depends_on = [kubernetes_cluster_role_binding.tiller_cluster_role_binding]
 }
