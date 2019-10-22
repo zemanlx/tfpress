aec57f1be3750a719fde92d990b291dc48d8b726
Vlastimil Zeman <zeman.vlastimil@gmail.com>
2019-10-22 09:12:19 +0100

    Add service account for Helm's tiller
    Solves a lot of issues with RBAC of Kubernetes:
    - unable to retrieve the complete list of server APIs:
      metrics.k8s.io/v1beta1: the server is currently unable to handle the
      request
    - configmaps is forbidden: User
      "system:serviceaccount:kube-system:default" cannot list resource
      "configmaps" in API group "" in the namespace "kube-system"

---
+++ kubernetes.tf
@@ -0,0 +1,27 @@
+resource "kubernetes_service_account" "tiller_service_account" {
+  metadata {
+    name      = "tiller"
+    namespace = "kube-system"
+  }
+}
+
+resource "kubernetes_cluster_role_binding" "tiller_cluster_role_binding" {
+  metadata {
+    name = "tiller"
+  }
+  role_ref {
+    api_group = "rbac.authorization.k8s.io"
+    kind      = "ClusterRole"
+    name      = "cluster-admin"
+  }
+  subject {
+    kind      = "ServiceAccount"
+    name      = "default"
+    namespace = "kube-system"
+  }
+  subject {
+    kind      = "ServiceAccount"
+    name      = "${kubernetes_service_account.tiller_service_account.metadata.0.name}"
+    namespace = "kube-system"
+  }
+}
--- providers.tf
+++ providers.tf
@@ -6,9 +6,20 @@ provider "google" {
 
 data "google_client_config" "current" {}
 
+provider "kubernetes" {
+  version = "~> 1.9"
+
+  host                   = google_container_cluster.default.endpoint
+  token                  = data.google_client_config.current.access_token
+  client_certificate     = base64decode(google_container_cluster.default.master_auth.0.client_certificate)
+  client_key             = base64decode(google_container_cluster.default.master_auth.0.client_key)
+  cluster_ca_certificate = base64decode(google_container_cluster.default.master_auth.0.cluster_ca_certificate)
+}
+
 provider "helm" {
-  version        = "~> 0.10"
-  install_tiller = true
+  version         = "~> 0.10"
+  install_tiller  = true
+  service_account = kubernetes_service_account.tiller_service_account.metadata.0.name
 
   kubernetes {
     host                   = google_container_cluster.default.endpoint
